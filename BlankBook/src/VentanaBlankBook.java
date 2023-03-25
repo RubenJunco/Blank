74260bb0fbc75664dc495d9b70812a9dd7ad6b5f
RubenJunco <128674160+RubenJunco@users.noreply.github.com>
2023-03-24 23:38:38 -0700

    puse una imagen

--- BlankBook/src/VentanaBlankBook.java
+++ BlankBook/src/VentanaBlankBook.java
@@ -1,4 +1,14 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 public class VentanaBlankBook extends JFrame  {
 
@@ -11,7 +21,38 @@ public class VentanaBlankBook extends JFrame  {
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setLocationRelativeTo(null);
 			
-	
+			
+			//panelCero
+			JPanel panel0 = new JPanel();	
+			this.getContentPane().add(panel0);
+			panel0.setLayout(null);
+			
+			//Primer panel
+			JPanel titulo = new JPanel();
+			titulo.setBackground(Color.decode("#747D96"));
+			this.getContentPane().add(titulo);
+			titulo.setLayout(null);
+			
+
+			JLabel DatosCliente = new JLabel("Power By Ruben Junco");
+			DatosCliente.setSize(250,50);
+			DatosCliente.setLocation(210,700);
+			DatosCliente.setFont(new Font("Comic Sans", Font.BOLD, 14));
+			titulo.add(DatosCliente);
+			
+			JButton save = new JButton();
+			save.setIcon(new ImageIcon("Enter.png")); //agregar una imagen
+			save.setBounds(100,130,400,400);
+			titulo.add(save);
+			
+			titulo.revalidate();
+			titulo.repaint();
+			
+			
+		}
+		
+		public void cambioPaneles() {
+			
 		}
 		
 		public static void main(String[] args) {
